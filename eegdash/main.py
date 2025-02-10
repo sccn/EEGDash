9ebe696dcf5731b2d70b9d2ce38f1ee306ca2a87
Arnaud Delorme <arnodelorme@gmail.com>
2025-02-10 11:53:50 -0800

    change to search MongDb directly

--- eegdash/main.py
+++ eegdash/main.py
@@ -2,16 +2,18 @@ from eegdash.signalstore_data_utils import SignalstoreBIDS
 
 class EEGDash:
     def __init__(self):
-        self.sstore = SignalstoreBIDS(
-            # dbconnectionstring='mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.1',
-            dbconnectionstring='mongodb+srv://eegdash-user:mdzoMjQcHWTVnKDq@cluster0.vz35p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-            is_public=True,
-            local_filesystem=False,
-            project_name='eegdash'
-        )
+        
+        DB_CONNECTION_STRING="mongodb+srv://eegdash-user:mdzoMjQcHWTVnKDq@cluster0.vz35p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        import pymongo
+        self.client = pymongo.MongoClient(DB_CONNECTION_STRING)
+        self.db = self.client['eegdash']
+        self.collection = self.db['records']   
     
     def find(self, *args):
-        return self.sstore.find(*args)
+        results = self.collection.find(*args)
+        
+        # convert to list using get_item on each element
+        return [result for result in results]
     
     def get(self, *args):
         
@@ -24,3 +26,11 @@ class EEGDash:
         # return BrainDecodeDataset(sessions)
         
         return self.sstore.get(*args)
+
+def main():
+    eegdash = EEGDash()
+    record = eegdash.find({'dataset': 'ds005511', 'subject': 'NDARUF236HM7'})
+    print(record)
+
+if __name__ == '__main__':
+    main()
