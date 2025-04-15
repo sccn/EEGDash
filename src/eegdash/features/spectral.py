24f89b1317ab911adbfdc81cfc72f6e72ff12723
avivdotan <avivd220@gmail.com>
2025-04-15 06:07:40 +0300

    spectral_bands_power, modified spectral_slope

--- src/eegdash/features/spectral.py
+++ src/eegdash/features/spectral.py
@@ -18,6 +18,12 @@ class NormalizedSpectralFeatureExtractor(ByChannelFeatureExtractor):
 
 
 @Feature(SpectralFeatureExtractor)
+class DBSpectralFeatureExtractor(ByChannelFeatureExtractor):
+    def preprocess(self, *x, **kwargs):
+        return (*x[:-1], 10 * np.log10(x[-1]))
+
+
+@Feature(SpectralFeatureExtractor)
 def root_total_power(f, p):
     return np.sqrt(p.sum(axis=-1))
 
@@ -44,14 +50,41 @@ def spectral_edge(f, p, edge=0.9):
     return se
 
 
-@Feature(SpectralFeatureExtractor)
+@Feature(DBSpectralFeatureExtractor)
 def spectral_slope(f, p):
+    log_f = np.log(f)
     exponent, intercept = np.empty(p.shape[0]), np.empty(p.shape[0])
     for i in range(p.shape[0]):
         ind = np.logical_and(f > 0, p[i] > 0)
         if ind.sum() > 1:
-            r = linregress(np.log(f[ind]), np.log(p[i, ind]))
+            r = linregress(log_f[ind], p[i, ind])
             exponent[i], intercept[i] = r.slope, r.intercept
         else:
             exponent[i], intercept[i] = np.nan, np.nan
     return {"exp": exponent, "int": intercept}
+
+
+@Feature(
+    SpectralFeatureExtractor,
+    NormalizedSpectralFeatureExtractor,
+    DBSpectralFeatureExtractor,
+)
+def spectral_bands_power(
+    f,
+    p,
+    bands={
+        "delta": (1, 4.5),
+        "theta": (4.5, 8),
+        "alpha": (8, 12),
+        "beta": (12, 30),
+    },
+):
+    bands_power = dict()
+    for k, v in bands.items():
+        assert isinstance(k, str)
+        assert isinstance(v, tuple)
+        assert len(v) == 2
+        mask = np.logical_and(f > v[0], f < v[1])
+        power = p[:, mask].sum(axis=-1)
+        bands_power[k] = power
+    return bands_power
