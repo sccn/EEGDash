e00d2cfb7cbb72405fdc1204fc5e682501b81842
Dung Truong <dt.young112@gmail.com>
2025-08-04 23:35:06 -0400

    update __init__

--- eegdash/__init__.py
+++ eegdash/__init__.py
@@ -1,7 +1,8 @@
 from .api import EEGDash, EEGDashDataset
 from .utils import __init__mongo_client
+from .dataset import EEGChallengeDataset
 
 __init__mongo_client()
 
-__all__ = ["EEGDash", "EEGDashDataset"]
+__all__ = ["EEGDash", "EEGDashDataset", "EEGChallengeDataset"]
 __version__ = "0.1.0"
