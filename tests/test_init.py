00ef0701e2b623f24c547b0dcb2ba0c154042e23
bruAristimunha <b.aristimunha@gmail.com>
2025-08-07 15:01:43 +0200

    updating to release

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,9 +1,10 @@
 from pathlib import Path
-
+import pytest
 from mne import get_config
 from torch.utils.data import Dataset
 
 from eegdash import EEGDash, EEGDashDataset
+import os
 
 cache_folder = Path(get_config("MNE_DATA"))
 
@@ -30,6 +31,7 @@ def test_dataset_api():
     assert isinstance(record, list)
 
 
+@pytest.mark.skipif("CI" in os.environ, reason="This test is only for local testing")
 def test_number_recordings():
     eeg_dash_instance = EEGDash()
 
