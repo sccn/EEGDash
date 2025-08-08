5d57245de8814a4c1ba247675253c9cc3fe96971
Pierre Guetschel <25532709+PierreGtch@users.noreply.github.com>
2025-08-08 14:35:47 +0200

    Allow `EEGChallengeDataset` to receive additional querry fields
    Fixes #37

--- eegdash/dataset.py
+++ eegdash/dataset.py
@@ -5,6 +5,7 @@ class EEGChallengeDataset(EEGDashDataset):
     def __init__(
         self,
         release: str = "R5",
+        additional_query: dict | None = None,
         cache_dir: str = ".eegdash_cache",
         s3_bucket: str | None = "s3://nmdatasets/NeurIPS25/R5_L100",
         **kwargs,
@@ -15,27 +16,19 @@ class EEGChallengeDataset(EEGDashDataset):
 
         Parameters
         ----------
-        query : dict | None
-            Optionally a dictionary that specifies the query to be executed; see
-            EEGDash.find() for details on the query format.
-        data_dir : str | list[str] | None
-            Optionally a string or a list of strings specifying one or more local
-            BIDS dataset directories from which to load the EEG data files. Exactly one
-            of query or data_dir must be provided.
-        dataset : str | list[str] | None
-            If data_dir is given, a name or list of names for for the dataset(s) to be loaded.
-        description_fields : list[str]
-            A list of fields to be extracted from the dataset records
-            and included in the returned data description(s). Examples are typical
-            subject metadata fields such as "subject", "session", "run", "task", etc.;
-            see also data_config.description_fields for the default set of fields.
+        release: str
+            Release name. Can be one of ["R1", ..., "R11"]
+        additional_query : dict | None
+            Optionally a dictionary that specifies a query to be executed, 
+            in addition to the dataset (automatically infered from the release argument. 
+            See EEGDash.find() for details on the query format.
         cache_dir : str
             A directory where the dataset will be cached locally.
         s3_bucket : str | None
             An optional S3 bucket URI to use instead of the
             default OpenNeuro bucket for loading data files.
         kwargs : dict
-            Additional keyword arguments to be passed to the EEGDashBaseDataset
+            Additional keyword arguments to be passed to the EEGDashDataset
             constructor.
 
         """
@@ -52,8 +45,11 @@ class EEGChallengeDataset(EEGDashDataset):
             "R2": "ds005506",
             "R1": "ds005505",
         }
+        query = additional_query or {}
+        assert "dataset" not in querry
+        querry["dataset"] = dsnumber_release_map[release] 
         super().__init__(
-            query={"dataset": dsnumber_release_map[release]},
+            query=query,
             cache_dir=cache_dir,
             s3_bucket=s3_bucket,
             **kwargs,
