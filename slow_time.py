378c76367d28e31a3238dd586f963f8145eec2b3
bruAristimunha <b.aristimunha@gmail.com>
2025-08-10 20:31:22 +0200

    renaming and moving the mongo connection

---
+++ slow_time.py
@@ -0,0 +1,8 @@
+import time
+from eegdash import EEGChallengeDataset
+
+#Measure time 
+
+dataset_obj = EEGChallengeDataset(release="R5")
+
+import ipdb; ipdb.set_trace()  # Set a breakpoint to inspect the dataset object
