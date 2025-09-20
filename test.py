0604e32164f6a91a25047602f03c9f4578f3cc3b
Bru <b.aristimunha@gmail.com>
2025-09-20 17:55:16 +0200

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from eegdash.dataset import EEGChallengeDataset
-from eegdash.paths import get_default_cache_dir
-
-cache_dir = get_default_cache_dir()
-dataset = EEGChallengeDataset(release="R5", cache_dir=cache_dir)
-
-print(dataset)
