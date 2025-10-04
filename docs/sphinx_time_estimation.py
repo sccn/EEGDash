f9c45196166904c4766c6612e72e2b19a6bbeada
bruAristimunha <b.aristimunha@gmail.com>
2025-10-05 00:44:22 +0200

    updating and pushing

--- docs/sphinx_time_estimation.py
+++ docs/sphinx_time_estimation.py
@@ -1,6 +1,16 @@
+from __future__ import annotations
+
 import math
 import re
+
 from docutils import nodes
+
+SKIP_CONTAINER_CLASSES = {
+    "sphx-glr-script-out",
+    "sphx-glr-single-img",
+    "sphx-glr-thumbnail",
+    "sphx-glr-horizontal",
+}
 
 
 class TextExtractor(nodes.NodeVisitor):
@@ -15,14 +25,41 @@ class TextExtractor(nodes.NodeVisitor):
         # Don't visit the children of literal blocks (i.e., code blocks)
         raise nodes.SkipNode
 
+    def visit_figure(self, node):
+        raise nodes.SkipNode
+
+    def visit_image(self, node):
+        raise nodes.SkipNode
+
+    def visit_container(self, node):
+        classes = set(node.get("classes", ()))
+        if classes & SKIP_CONTAINER_CLASSES:
+            raise nodes.SkipNode
+
     def unknown_visit(self, node):
         """Pass for all other nodes."""
         pass
 
 
+EXAMPLE_PREFIX = "generated/auto_examples/"
+
+
+def _should_calculate(pagename: str) -> bool:
+    if not pagename:
+        return False
+    if not pagename.startswith(EXAMPLE_PREFIX):
+        return False
+    if pagename.endswith("/sg_execution_times"):
+        return False
+    if pagename == "generated/auto_examples/index":
+        return False
+    return True
+
+
 def html_page_context(app, pagename, templatename, context, doctree):
-    """Add estimated reading time to the template context."""
-    if not doctree:
+    """Add estimated reading time directly under tutorial titles."""
+    if not doctree or not _should_calculate(pagename):
+        context.pop("reading_time", None)
         return
 
     visitor = TextExtractor(doctree)
@@ -34,7 +71,29 @@ def html_page_context(app, pagename, templatename, context, doctree):
     wpm = 200  # Median reading speed
     reading_time = math.ceil(word_count / wpm) if wpm > 0 else 0
 
+    if reading_time <= 0:
+        context.pop("reading_time", None)
+        return
+
     context["reading_time"] = reading_time
+
+    body = context.get("body")
+    if not isinstance(body, str) or "</h1>" not in body:
+        return
+
+    minutes_label = "minute" if reading_time == 1 else "minutes"
+    badge_html = (
+        '<div class="eegdash-reading-time" role="note">'
+        '<span class="eegdash-reading-time__label">Estimated reading time:</span>'
+        f'<span class="eegdash-reading-time__value">{reading_time} {minutes_label}</span>'
+        "</div>"
+    )
+
+    insert_at = body.find("</h1>")
+    if insert_at == -1:
+        return
+
+    context["body"] = body[: insert_at + 5] + badge_html + body[insert_at + 5 :]
 
 
 def setup(app):
